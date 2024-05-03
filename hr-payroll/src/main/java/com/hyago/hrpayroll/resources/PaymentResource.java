92af147a79902caa635f102da0fb34611e018448
C1326348 Hyago Souza <C1326348@interno.bb.com.br>
2024-05-03 17:58:14 -0300

    Hystrix fault tolerance

--- hr-payroll/src/main/java/com/hyago/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/hyago/hrpayroll/resources/PaymentResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.hyago.hrpayroll.entities.Payment;
 import com.hyago.hrpayroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 @RequestMapping(value = "/payments")
@@ -18,9 +19,16 @@ public class PaymentResource {
 	private PaymentService service;
 
 	
+	@HystrixCommand(fallbackMethod = "getPaymentAlternative")
 	@GetMapping(value= "/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
 		Payment payment = service.getPayment(workerId, days);
+		return ResponseEntity.ok(payment);
+		
+	}
+	
+	public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days){
+		Payment payment = new Payment("Brann", 400.0,days);
 		return ResponseEntity.ok(payment);
 		
 	}
