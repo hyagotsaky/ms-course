698d0257b9773d596357b2a4fc5c0fba4523818e
C1326348 Hyago Souza <C1326348@interno.bb.com.br>
2024-05-13 14:31:28 -0300

    Login, JWT token

---
+++ hr-oauth/src/main/java/com/hyago/hroauth/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.hyago.hroauth.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+
+//vamos sobescrever um metodo da classe WebSecurityConfigurerAdapter
+
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+	@Override
+	@Autowired
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+	}
+	
+
+	@Override
+	@Bean
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
+	
+
+	
+
+}
